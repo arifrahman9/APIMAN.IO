ec2d6585e534d43bb3717f0db27ee167e4bcb783
arifrahman9 <arifrhmn910@gmail.com>
2021-11-12 17:52:42 +0700

    create login-google

--- server/models/users-model.js
+++ server/models/users-model.js
@@ -1,79 +1,96 @@
-const mongodb = require('mongodb');
-const { getDatabase } = require('../config/mongo');
-const { hashPassword } = require('../helpers/bcrypt');
+const mongodb = require("mongodb")
+const { getDatabase } = require("../config/mongo")
+const { hashPassword } = require("../helpers/bcrypt")
 
 class UsersModel {
   static async register(reqBody) {
-    const { username, email, password, firstName, lastName } = reqBody;
+    const { username, email, password, firstName, lastName } = reqBody
 
-    const db = getDatabase();
-    const usersCollection = db.collection('users');
+    const db = getDatabase()
+    const usersCollection = db.collection("users")
     const user = await usersCollection.insertOne({
       username,
       email,
       password: hashPassword(password),
       firstName,
       lastName,
-    });
+    })
 
-    return user;
+    return user
   }
 
   static async login(reqBody) {
-    const { email, username } = reqBody;
-    const db = getDatabase();
-    const usersCollection = db.collection('users');
+    const { email, username } = reqBody
+    const db = getDatabase()
+    const usersCollection = db.collection("users")
 
-    let findOneOption;
+    let findOneOption
 
     if (email) {
       findOneOption = {
         email,
-      };
+      }
     } else {
       findOneOption = {
         username,
-      };
+      }
     }
 
-    const response = await usersCollection.findOne(findOneOption);
+    const response = await usersCollection.findOne(findOneOption)
 
-    return response;
+    return response
   }
 
   static async getLoggedInUser(id) {
-    const db = getDatabase();
-    const usersCollection = db.collection('users');
+    const db = getDatabase()
+    const usersCollection = db.collection("users")
 
     const foundUser = await usersCollection.findOne({
       _id: mongodb.ObjectId(id),
-    });
+    })
 
-    return foundUser;
+    return foundUser
   }
 
   static async findUserByEmail(email) {
-    const db = getDatabase();
-    const usersCollection = db.collection('users');
+    const db = getDatabase()
+    const usersCollection = db.collection("users")
 
     const response = await usersCollection.findOne({
       email,
-    });
+    })
 
-    return response;
+    return response
   }
 
   static async getLastInsertedUser() {
-    const db = getDatabase();
-    const usersCollection = db.collection('users');
-    const user = await usersCollection
-      .find({})
-      .sort({ _id: -1 })
-      .limit(1)
-      .toArray();
+    const db = getDatabase()
+    const usersCollection = db.collection("users")
+    const user = await usersCollection.find({}).sort({ _id: -1 }).limit(1).toArray()
 
-    return user;
+    return user
+  }
+
+  static async loginGoogle(reqBody) {
+    console.log(reqBody, "google login")
+    const emailFromGoogle = reqBody.email
+    const nameFromGoogle = reqBody.name
+    const firstNameFromGoogle = reqBody.firstName
+    const lastNameFromGoogle = reqBody.lastName
+    const db = getDatabase()
+    const usersCollection = db.collection("users")
+
+    let findOrCreate = emailFromGoogle
+      ? usersCollection.findOne({ email: emailFromGoogle })
+      : usersCollection.insertOne({
+          username: nameFromGoogle,
+          email: emailFromGoogle,
+          password: hashPassword(Math.random().toString(36).slice(-8)),
+          firstName: firstNameFromGoogle,
+          lastName: lastNameFromGoogle,
+        })
+    return findOrCreate
   }
 }
 
-module.exports = UsersModel;
+module.exports = UsersModel

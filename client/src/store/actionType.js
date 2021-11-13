b0d83dbfafb3c0cd814ef30b6c1b53d33ed514ca
rasyidrmhd <rasyidrmuhammad@gmail.com>
2021-11-14 00:19:31 +0700

    working on collection tab

--- client/src/store/actionType.js
+++ client/src/store/actionType.js
@@ -1,2 +1,3 @@
 export const SET_USERDATA = "userdata/setUserdata";
-export const SET_HISTORIES = "histories/setHistories"
+export const SET_COLLECTIONS = "collections/setCollections";
+export const SET_HISTORIES = "histories/setHistories";

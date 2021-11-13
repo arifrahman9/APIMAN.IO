940b6d89682a2df90eaa7b0ef5ad08a9ebfb1cd2
Darwin <darwinsantoso@yahoo.com>
2021-11-13 13:43:03 +0700

    ✅ collections endpoint testing done

--- server/__tests__/collections.test.js
+++ server/__tests__/collections.test.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
 const mongodb = require('mongodb');
-const { hashPassword } = require('../helpers/bcrypt');
 const { connect, getDatabase } = require('../config/mongo');
 
 let newestCollection;
@@ -16,14 +15,6 @@ describe('GET /collections', () => {
 
     db = getDatabase();
     const usersCollection = db.collection('users');
-
-    // await usersCollection.insertOne({
-    //   username: 'testuser',
-    //   email: 'test@gmail.com',
-    //   password: hashPassword('password'),
-    //   firstName: 'firstTest',
-    //   lastName: 'lastTest',
-    // });
 
     await request(app).post('/register').send({
       username: 'testuser',
@@ -87,7 +78,6 @@ describe('GET /collections', () => {
   });
 
   test('[success] successfuly fetched collections that belongs to the user', (done) => {
-    console.log(loginResponse.body.access_token, '<=====');
     request(app)
       .get('/collections')
       .set({
@@ -100,6 +90,25 @@ describe('GET /collections', () => {
         expect(body[0]).toHaveProperty('_id');
         expect(body[0]).toHaveProperty('name');
         expect(body[0]).toHaveProperty('UserId');
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('[failed] failed to fetch collections', (done) => {
+    request(app)
+      .get('/collections')
+      .set({
+        access_token: loginResponse.body.access_token + 'asd',
+      })
+      .then((response) => {
+        const body = response.body;
+
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty('message');
 
         done();
       })
@@ -121,6 +130,424 @@ describe('GET /collections', () => {
         expect(body).toHaveProperty('_id');
         expect(body).toHaveProperty('name');
         expect(body).toHaveProperty('UserId');
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('[failed] failed to fetch collection by id because of invalid token', (done) => {
+    request(app)
+      .get(`/collections/${newestCollection[0]._id}`)
+      .set({
+        access_token: loginResponse.body.access_token + 'asd',
+      })
+      .then((response) => {
+        const body = response.body;
+
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty('message');
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('[failed] failed to fetch collection by id because of empty id', (done) => {
+    request(app)
+      .get(`/collections/${null}`)
+      .set({
+        access_token: loginResponse.body.access_token,
+      })
+      .then((response) => {
+        const body = response.body;
+
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty('message');
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
+
+describe('PATCH /collections', () => {
+  beforeAll(async () => {
+    await connect();
+    await getDatabase();
+
+    db = getDatabase();
+    const usersCollection = db.collection('users');
+
+    await request(app).post('/register').send({
+      username: 'testuser',
+      email: 'test@gmail.com',
+      password: 'password',
+      firstName: 'firstTest',
+      lastName: 'lastTest',
+    });
+
+    lastInsertedUser = await usersCollection
+      .find({})
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+
+    console.log(lastInsertedUser);
+
+    // then login
+    const payload = {
+      email: 'test@gmail.com',
+      password: 'password',
+    };
+
+    loginResponse = await request(app).post('/login').send(payload);
+
+    console.log(loginResponse.body.access_token);
+
+    db = getDatabase();
+    const collectionsCollection = db.collection('collections');
+
+    await collectionsCollection.insertOne({
+      name: 'new test collection',
+      UserId: lastInsertedUser[0]._id,
+    });
+
+    newestCollection = await collectionsCollection
+      .find({})
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+
+    console.log(newestCollection);
+  });
+
+  afterAll(async () => {
+    await connect();
+    await getDatabase();
+
+    db = getDatabase();
+    const collectionsCollection = db.collection('collections');
+
+    await collectionsCollection.deleteOne({
+      _id: newestCollection[0]._id,
+    });
+
+    const usersCollection = db.collection('users');
+
+    await usersCollection.deleteOne({
+      _id: lastInsertedUser[0]._id,
+    });
+  });
+
+  test('[success] change collection name successfull', (done) => {
+    request(app)
+      .patch('/collections')
+      .set({
+        access_token: loginResponse.body.access_token,
+      })
+      .send({
+        id: newestCollection[0]._id,
+        name: 'new name',
+      })
+      .then((response) => {
+        const body = response.body;
+
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty('_id');
+        expect(body).toHaveProperty('name', 'new name');
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('[failed] change collection name failed', (done) => {
+    request(app)
+      .patch('/collections')
+      .set({
+        access_token: loginResponse.body.access_token,
+      })
+      .send({
+        id: '',
+        name: 'new name',
+      })
+      .then((response) => {
+        const body = response.body;
+
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty('message', 'Internal Server Error');
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
+
+describe('DELETE /collections', () => {
+  beforeAll(async () => {
+    await connect();
+    await getDatabase();
+
+    db = getDatabase();
+    const usersCollection = db.collection('users');
+
+    await request(app).post('/register').send({
+      username: 'testuser',
+      email: 'test@gmail.com',
+      password: 'password',
+      firstName: 'firstTest',
+      lastName: 'lastTest',
+    });
+
+    lastInsertedUser = await usersCollection
+      .find({})
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+
+    console.log(lastInsertedUser);
+
+    // then login
+    const payload = {
+      email: 'test@gmail.com',
+      password: 'password',
+    };
+
+    loginResponse = await request(app).post('/login').send(payload);
+
+    console.log(loginResponse.body.access_token);
+
+    db = getDatabase();
+    const collectionsCollection = db.collection('collections');
+
+    await collectionsCollection.insertOne({
+      name: 'new test collection',
+      UserId: lastInsertedUser[0]._id,
+    });
+
+    newestCollection = await collectionsCollection
+      .find({})
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+
+    console.log(newestCollection);
+  });
+
+  afterAll(async () => {
+    await connect();
+    await getDatabase();
+
+    db = getDatabase();
+    const collectionsCollection = db.collection('collections');
+
+    // await collectionsCollection.deleteOne({
+    //   _id: newestCollection[0]._id,
+    // });
+
+    const usersCollection = db.collection('users');
+
+    await usersCollection.deleteOne({
+      _id: lastInsertedUser[0]._id,
+    });
+  });
+
+  test('[success] delete collection successfull', (done) => {
+    request(app)
+      .delete('/collections')
+      .set({
+        access_token: loginResponse.body.access_token,
+      })
+      .send({
+        id: newestCollection[0]._id,
+        name: 'new name',
+      })
+      .then((response) => {
+        const body = response.body;
+
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty('_id');
+        expect(body).toHaveProperty('name');
+        expect(body).toHaveProperty('UserId');
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('[failed] delete collection failed', (done) => {
+    request(app)
+      .patch('/collections')
+      .set({
+        access_token: loginResponse.body.access_token,
+      })
+      .send({
+        id: '',
+      })
+      .then((response) => {
+        const body = response.body;
+
+        console.log(body);
+
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty('message', 'Internal Server Error');
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('[failed] delete collection failed because of empty id', (done) => {
+    request(app)
+      .patch('/collections')
+      .set({
+        access_token: loginResponse.body.access_token,
+      })
+      .send({
+        id: '',
+      })
+      .then((response) => {
+        const body = response.body;
+
+        console.log(body);
+
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty('message');
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
+
+describe('POST /collections', () => {
+  beforeAll(async () => {
+    await connect();
+    await getDatabase();
+
+    db = getDatabase();
+    const usersCollection = db.collection('users');
+
+    await request(app).post('/register').send({
+      username: 'testuser',
+      email: 'test@gmail.com',
+      password: 'password',
+      firstName: 'firstTest',
+      lastName: 'lastTest',
+    });
+
+    lastInsertedUser = await usersCollection
+      .find({})
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+
+    // then login
+    const payload = {
+      email: 'test@gmail.com',
+      password: 'password',
+    };
+
+    loginResponse = await request(app).post('/login').send(payload);
+
+    console.log(loginResponse.body.access_token);
+
+    db = getDatabase();
+    const collectionsCollection = db.collection('collections');
+
+    await collectionsCollection.insertOne({
+      name: 'new test collection',
+      UserId: lastInsertedUser[0]._id,
+    });
+
+    newestCollection = await collectionsCollection
+      .find({})
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+  });
+
+  afterAll(async () => {
+    await connect();
+    await getDatabase();
+
+    db = getDatabase();
+    const collectionsCollection = db.collection('collections');
+
+    await collectionsCollection.deleteOne({
+      _id: newestCollection[0]._id,
+    });
+
+    const usersCollection = db.collection('users');
+
+    await usersCollection.deleteOne({
+      _id: lastInsertedUser[0]._id,
+    });
+  });
+
+  test('[success] add new collection successful', (done) => {
+    request(app)
+      .post('/collections')
+      .set({
+        access_token: loginResponse.body.access_token,
+      })
+      .send({
+        name: 'newest collection',
+      })
+      .then((response) => {
+        const body = response.body;
+
+        expect(body).toEqual(expect.any(Array));
+        expect(body[0]).toHaveProperty('_id');
+        expect(body[0]).toHaveProperty('name');
+        expect(body[0]).toHaveProperty('UserId');
+
+        db = getDatabase();
+        const collectionsCollection = db.collection('collections');
+
+        collectionsCollection.deleteOne({
+          _id: mongodb.ObjectId(body[0]._id),
+        });
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('[failed] add new collection failed', (done) => {
+    request(app)
+      .post('/collections')
+      .set({
+        access_token: loginResponse.body.access_token,
+      })
+      .send({})
+      .then((response) => {
+        const body = response.body;
+
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty(
+          'message',
+          'collection name cannot be empty'
+        );
 
         done();
       })

1564a9d9ffcf3c3d7006245f56fcea1456161681
Darwin <darwinsantoso@yahoo.com>
2021-11-14 20:41:33 +0700

    🚀 heroku deploy setup

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,5 +1,15 @@
 const Redis = require('ioredis');
+let redis;
 
-const redis = new Redis();
+if (process.env.NODE_ENV !== 'production') {
+  redis = new Redis();
+} else {
+  console.log('pake redis labs');
+  redis = new Redis({
+    port: process.env.REDIS_LAB_PORT,
+    host: process.env.REDIS_LAB_URL,
+    password: process.env.REDIS_LAB_PASSWORD,
+  });
+}
 
 module.exports = redis;
